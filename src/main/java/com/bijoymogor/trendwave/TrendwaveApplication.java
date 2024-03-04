7fc190ea787be5cae762f6e848715ef3d341cbeb
Bijoy <bijoymogor@gmail.com>
2024-03-05 00:48:37 +0530

    Project Initialization

---
+++ src/main/java/com/bijoymogor/trendwave/TrendwaveApplication.java
@@ -0,0 +1,13 @@
+package com.bijoymogor.trendwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendwaveApplication.class, args);
+	}
+
+}
